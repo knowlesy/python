b2feb4481118e752fae4a359a6ae5d635df01738
Pete K <20459678+knowlesy@users.noreply.github.com>
2023-12-08 11:21:25 +0000

    Create maths.py

---
+++ Basics/maths.py
@@ -0,0 +1,14 @@
+# + / * - %
+age1 = 12
+age2 = 18
+
+print(age1 + age2)
+
+print (age1 - age2)
+
+sent = "The cat walked down the road"
+# chars / indexing 
+print(sent[0])
+print(sent[8])
+# slicing range of 
+print(sent[0:6])
