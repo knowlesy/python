0ae57f9973968ab1f376d427f66479f021a50458
Pete K <20459678+knowlesy@users.noreply.github.com>
2023-12-08 12:23:16 +0000

    Create for loops.py

---
+++ Basics/for loops.py
@@ -0,0 +1,22 @@
+list1 = ['milk','bread','apples','yoghurt']
+
+for item in list1:
+    print(item)
+
+for i in range(0, 10):
+    print(i)
+
+#notice differences in range printed 
+for i in range(1, 11):
+    print(i)
+
+# increments of 2 
+for i in range(0, 10, 2):
+    print(i)
+
+for i in range(9, 53, 5):
+    print(i)
+
+for i in range (0, 5):
+    for j in range (0,3):
+        print(i * j)
