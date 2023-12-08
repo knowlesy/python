cd4eeb71a0162a2e15762df5554b03018f181ac6
Pete K <20459678+knowlesy@users.noreply.github.com>
2023-12-08 15:11:50 +0000

    Create python funct.py

---
+++ Basics/python funct.py
@@ -0,0 +1,18 @@
+# absolute value 
+print(abs(-5))
+
+#boolean
+print(bool(2))
+print(bool(None))
+print(bool(-2))
+print(bool(0))
+
+#dir - The dir() function returns all properties and methods of the specified object, without the values.
+print(dir("hello"))
+
+#help
+print(help("hello".upper))
+
+
+print ("hello " + str(123))
+print (123 + int("456"))
