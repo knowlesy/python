bf678ae788e239bda394a0849c5cf62d02132939
Pete K <20459678+knowlesy@users.noreply.github.com>
2023-12-08 13:14:12 +0000

    Create functions.py

---
+++ Basics/functions.py
@@ -0,0 +1,21 @@
+def hello_world():
+    print("hello  world!")
+
+
+hello_world()
+
+
+def greeting(name):
+    print("Hi " + name + " !")
+
+greeting("Jim")
+
+def add(num1, num2):
+         return num1 + num2
+
+add(10, 15)
+
+def mul(num1, num2):
+      return num1 * num2
+
+print(mul(add(1,2), add(3,4)))
