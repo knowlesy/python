d6400ad58c946202c1ca951be7866e48cf5b2a37
Pete K <20459678+knowlesy@users.noreply.github.com>
2023-12-22 09:47:54 +0000

    Update tuple.py

--- Basics/tuple.py
+++ Basics/tuple.py
@@ -1,6 +1,17 @@
+#lists but are immutable 
 tup = ('oranges', 'apples', ' cherrys')
 print (tup)
 print (tup[0])
 
 # wont work as it cant be modified 
 tup[0] = 'milk'
+
+#can print
+tup = ('oranges', 'apples', ' cherrys')
+print(tup[0])
+
+
+tup2 = (11,13)
+#can join 
+tup3 = tup + tup2
+print (tup3)
